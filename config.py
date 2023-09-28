3aced2b9e0850536b07c20d318fb36b601205740
SirAlbaToR <90750816+SirAlbaToR@users.noreply.github.com>
2023-09-29 00:17:14 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://postgres:qwerty1234@localhost'
-SECRET_KEY = 'qwerty1234'
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_ECHO = False
-ADMIN_ROLE_ID = 1
-MODER_ROLE_ID = 2
-
